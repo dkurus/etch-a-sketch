d3dc775b8d100560efa620d736a9cb298167e339
Damiann Kurus <Damiannkurus@gmail.com>
2021-06-23 19:59:43 -0400

    works up to 25 length

--- script.js
+++ script.js
@@ -1,30 +1,56 @@
 const gridContainer = document.querySelector('.container');
-
+let allTheCells = document.querySelectorAll('.cells');
 function create16Divs () {
     for (let i = 0; i < 256; i++) {
      
      let divSelector = document.createElement('div');
      divSelector.className = 'cells';
      gridContainer.appendChild(divSelector);
-     
     }
+    colorsTheCells();
 }
-
 create16Divs();
 
-const allTheCells = document.querySelectorAll('.cells');
-// allTheCells.forEach('mouseover', e => {
-//     e.target.style.color = 'black';
-// })
-
-allTheCells.forEach((item) => {
+function colorsTheCells () {
+    let allTheCells = document.querySelectorAll('.cells');
+    allTheCells.forEach((item) => {
     item.addEventListener('mouseover', e => {
         e.target.style.background = 'black';
     })
 })
+}
 
 const resetButton = document.querySelector('button');
 resetButton.addEventListener('click', () => {
-    
+    allTheCells.forEach((item) => {
+        item.style.background = 'white';    
+    })
+
+  const userCellNumberInput = prompt('please decide how many cells the width should be \r\n(max 100)', 100);
+
+  gridResizer(userCellNumberInput);
 })
 
+function gridResizer (userInput) {
+    removeAllCellsForRefresh();
+    let userInput2Num = Number(userInput);
+    let totalCellsNeeded = userInput2Num * userInput2Num;
+    
+    let cellSizeCalc = (100*(1 / userInput2Num))
+    gridContainer.style.gridTemplateColumns = `repeat(${userInput2Num},${cellSizeCalc}%)`;
+
+    for (let i = 0; i < totalCellsNeeded; i++){
+        let divSelector = document.createElement('div');
+        divSelector.className = 'cells';
+        gridContainer.appendChild(divSelector);
+        colorsTheCells();
+    }
+}
+
+function removeAllCellsForRefresh () {
+    const everyCell = document.querySelectorAll('.cells')
+    everyCell.forEach((cell)=> {
+        gridContainer.removeChild(cell);
+    })
+}
+    
